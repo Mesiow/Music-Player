9b52edc8ffc49375edf27affcc2f59e5c16ae8ab
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-02 12:15:15 -0500

    add music pop up list with styles

--- src/Components/MusicList.js
+++ src/Components/MusicList.js
@@ -1,15 +1,75 @@
 import React, {Component} from 'react';
 import '../styles/MusicList.css';
+import egems from '../images/egems.png';
+
+const music = [
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    },
+    {
+        img: egems,
+        title:"Home",
+        artist: "Resonance"
+    }
+]
 
 
 class MusicList extends Component{
     constructor(props){
         super(props);
-        this.state = { songs: [] };
+        this.state = { songs: music };
     }
     render(){
         return(
             <div className="List-Container">
+                {this.state.songs.map((item) => {
+                return (
+                 <div className="Song-Item">
+                     <img className="song-img" src={item.img}/>
+                     <div className="Meta">
+                         <span className="song-title-item">{item.title}</span>
+                         <span className="song-artist-item">{item.artist}</span>
+                     </div>
+                 </div>
+                );
+                })}
                 
             </div>
         );
