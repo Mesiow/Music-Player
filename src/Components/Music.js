c46b5e183997418e29cca36aa290bc492bc3de91
Chris <34993144+Mesiow@users.noreply.github.com>
2020-12-05 20:29:23 -0500

    update music list

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -1,7 +1,29 @@
 import egems from '../images/egems.png';
 import HOME from '../Music/HOME-Resonance.mp3';
+
 import youth from '../images/youth.jpg';
 import YOUTH from '../Music/Krosia-Youth.mp3';
+
+import mimosa from '../images/mimosa.jpg';
+import MIMOSA from '../Music/Krosia-Mimosa.mp3';
+
+import dive from '../images/dive.jpg';
+import DIVE from '../Music/Krosia-Dive.mp3';
+
+import phase from '../images/phase.jpg';
+import PHASE from '../Music/Hotel Pools-Phase.mp3';
+
+import dazed from '../images/dazed.jpg';
+import DAZED from '../Music/Diosca-Dazed.mp3';
+
+import sun from '../images/sun.jpg';
+import SUN from '../Music/Sun.mp3'
+
+import slow from '../images/slow.jpg';
+import SLOW from '../Music/Slow.mp3';
+
+import dream from '../images/DREAM.jpg';
+import DREAM from '../Music/HOME-Dream Head.mp3';
 
 const music = [
     {
@@ -17,39 +39,55 @@ const music = [
         title:"Youth",
         artist:"Krosia"
     },
-/*
-    {
 
+    {
+        img: mimosa,
+        audio:MIMOSA,
+        title:"Mimosa",
+        artist:"Krosia"
     },
 
     {
-
+        img: dive,
+        audio:DIVE,
+        title:"Dive",
+        artist:"Krosia"
     },
 
     {
-
+        img: phase,
+        audio:PHASE,
+        title:"Phase",
+        artist:"Hotel Pools"
     },
 
     {
-
+        img: dazed,
+        audio:DAZED,
+        title:"Dazed",
+        artist:"Diosca"
     },
 
     {
-
+        img: sun,
+        audio:SUN,
+        title:"Sun",
+        artist:"憂鬱"
     },
 
     {
-
+        img: slow,
+        audio:SLOW,
+        title:"Slow",
+        artist:"憂鬱"
     },
 
     {
-
+        img: dream,
+        audio:DREAM,
+        title:"Dream Head",
+        artist:"HOME"
     },
-
-    {
-
-    },
-    */
 ]
 
 export {music};
